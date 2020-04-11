e37d35cd3edb2c980413905e5254e64c3a4e96ed
ShunOmine <shun12baseball@gmail.com>
2020-04-11 11:46:48 +0900

    add: swagger-uiの導入

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 
 const app: express.Express = express();
+const swagger = require('swagger-ui-dist').absolutePath();
+const swaggerJSDoc = require('swagger-jsdoc');
 
 // CORSの許可
 app.use((req, res, next) => {
@@ -12,15 +14,38 @@ app.use((req, res, next) => {
 // body-parserに基づいた着信リクエストの解析
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// swagger-uiを表示するパス
+app.use('/swagger', express.static(swagger));
+
+// swagger-jsdocの設定
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: 'Sample App',
+      version: '1.0.0'
+    },
+    basePath: '/api',
+    url: "http://localhost:3000/api-docs.json",
+    schemes: [ 'http' ]
+  },
+  apis: ['./openapi.yml']
+}
+const swaggerSpec = swaggerJSDoc(options);
 
 // ルーティング
-const router : express.Router = express.Router();
+const router: express.Router = express.Router();
 router.get('/api/getTest', (req: express.Request, res: express.Response) => {
   res.send(req.query);
 });
 
 router.post('/api/postTest', (req: express.Request, res: express.Response) => {
   res.send(req.body);
+});
+
+// swagger-jsdoc用のjsonを吐き出すroute
+router.get('/api-docs.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
 });
 
 app.use(router);
